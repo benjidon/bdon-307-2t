c04b4695ebad37cf00c9ae1b1024513596881421
benjidon466 <32624070+benjidon466@users.noreply.github.com>
2019-02-06 10:18:38 -0800

    Add files via upload

---
+++ bdon-307-1T/src/main/java/test/test2/App.java
@@ -0,0 +1,18 @@
+package test.test2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World! yay" );
+        multiply(2,3);
+    }
+
+    public static int multiply(int i, int k) {
+    	return i * k;
+    }
+}
